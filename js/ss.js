3bfaa7a38a668007d572c05022f2c64f4d8e9468
Douglas Rocha <douglas.rocha@americanas.delivery>
2023-05-08 21:24:04 -0300

    WIP

--- js/ss.js
+++ js/ss.js
@@ -1,17 +1,21 @@
 
 // Cria um array com os jogos disponíveis no site
   // Aqui são só exemplos.
-const jogosDisponiveis = [
-    { nome: "Super Mario World", link: "SuperMarioWorld.html", img: "../img/super-mario-world-icon.jfif" },
-    { nome: "Call of Duty", link: "SuperMarioWorld.html", img: "../img/super-mario-world-icon.jfif" },
-    { nome: "Minecraft", link: "SuperMarioWorld.html", img: "../img/super-mario-world-icon.jfif" },
-    { nome: "FIFA 22", link: "SuperMarioWorld.html", img: "../img/super-mario-world-icon.jfif" },
-    { nome: "Assassin's Creed Valhalla", link: "SuperMarioWorld.html", img: "../img/super-mario-world-icon.jfif" },
-    { nome: "Among Us", link: "SuperMarioWorld.html", img: "../img/super-mario-world-icon.jfif" },
-    { nome: "Sonic3", link: "SuperMarioWorld.html", img: "../img/super-mario-world-icon.jfif" },
-    { nome: "Tetris", link: "SuperMarioWorld.html", img: "../img/super-mario-world-icon.jfif" },
-    { nome: "GTA2", link: "SuperMarioWorld.html", img: "../img/super-mario-world-icon.jfif" },
-    { nome: "PokemonEmerald", link: "SuperMarioWorld.html", img: "../img/super-mario-world-icon.jfif" }
+  const jogosDisponiveis = [
+    { nome: "Super Mario World", link: "https://www.jogosfas.com/game.php?id_juego=19435&code=68v7988vvb9vvvv6c277bee9vbv2vv98", img: "../img/super-mario-world-icon.jfif" },
+    { nome: "Adivinhe o Pokémon", link: "https://www.jogosfas.com/game.php?id_juego=11528&code=68v7988vvb9vvvv6c277bee9vbv2vv98", img: "../img/adivinhe-pokemon-icon.webp" },
+    { nome: "Donkey Kong Arcade", link: "https://www.jogosfas.com/game.php?id_juego=20816&code=68v7988vvb9vvvv6c277bee9vbv2vv98", img: "../img/donkey-kong-country.webp" },
+    { nome: "GTA 2", link: "https://www.jogosfas.com/game.php?id_juego=20740&code=68v7988vvb9vvvv6c277bee9vbv2vv98", img: "../img/gta 2.jpg" },
+    { nome: "Subway Surfers", link: "https://www.jogosfas.com/game.php?id_juego=21244&code=68v7988vvb9vvvv6c277bee9vbv2vv98", img: "../img/subway-surfers.webp" },
+    { nome: "CrashBandicoot KR", link: "https://www.jogosfas.com/game.php?id_juego=18298&code=68v7988vvb9vvvv6c277bee9vbv2vv98", img: "../img/crash-bandicoot-icon.jfif" },
+    { nome: "Among Us Run Christmas", link: "https://www.jogosfas.com/game.php?id_juego=20033&code=68v7988vvb9vvvv6c277bee9vbv2vv98", img: "../img/among-us.jpg" },
+    { nome: "Pokemon Emerald", link: "https://www.jogosfas.com/game.php?id_juego=18053&code=68v7988vvb9vvvv6c277bee9vbv2vv98", img: "../img/pk-emerald-icon.jfif" },
+    { nome: "Sonic 3", link: "https://www.jogosfas.com/game.php?id_juego=18028&code=68v7988vvb9vvvv6c277bee9vbv2vv98", img: "../img/sonic3-icon.jpg" },
+    { nome: "Sonic Hedgehog", link: "https://www.jogosfas.com/game.php?id_juego=19847&code=68v7988vvb9vvvv6c277bee9vbv2vv98", img: "../img/sonic-hedgehog-icon.jfif" },
+    { nome: "Surfers Peru", link: "https://www.jogosfas.com/game.php?id_juego=21320&code=68v7988vvb9vvvv6c277bee9vbv2vv98", img: "../img/surfers-peru-icon.png" },
+    { nome: "Vestir Garota", link: "https://www.jogosfas.com/game.php?id_juego=21230&code=68v7988vvb9vvvv6c277bee9vbv2vv98", img: "../img/vestirGarotasDeAnime-icon.webp" },
+    { nome: "Sniper Shot", link: "https://html5.gamedistribution.com/25747b9934954278ba356dce7d886ab9/?gd_sdk_referrer_url=https://www.clickjogos.com.br/jogos/sniper-shot", img: "../img/sniper-shot-icon.jpg" },
+    { nome: "Fireboy Watergirl", link: "https://html5.gamedistribution.com/03f124074b0049f89fdace0cacf8ead5/?gd_sdk_referrer_url=https://www.clickjogos.com.br/jogos/fireboy-and-watergirl-1-forest-temple", img: "../img/firewater-icon.png" },
 ];
 
 // Define o número máximo de jogos recomendados
