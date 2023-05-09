3213a5bcbf0564a8b3f5bb79115d25d2296e2d92
Victor Aquilla <victor.aquilla07@gmail.com>
2023-05-08 21:26:36 -0300

    g

--- js/game-template.js
+++ js/game-template.js
@@ -5,7 +5,9 @@ let iframe =   localStorage.getItem("linkGame");
 
 const nameGameElement = document.querySelector(".title-game");
 const iframeElement = document.querySelector(".iframe-game");
+document.title = nameGame
 
 
 nameGameElement.innerHTML = nameGame;
 iframeElement.src = iframe;
+document.title = nameGame;
